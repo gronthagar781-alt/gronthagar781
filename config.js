3b929871fda64910cdcc60db85ec64736c8c143c
gronthagar781-alt <gronthagar781@gmail.com>
2026-08-22 08:12:25 +0530

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-export const SUPABASE_URL='https://uaomovfjtolymgjdnizw.supabase.co';
-export const SUPABASE_PUBLISHABLE_KEY='sb_publishable_U8v1R8CcbLWl1LTtEhaIxQ_Puf30oeJ';
-export const ADMIN_UID='ba8bf619-3f66-42b8-8963-408bac70349c';
-export const DRIVE_FOLDERS=['1JxRjGjW0MhhD8x6K4LDZ2zaOAWyotsAz','1uKJxARjapuI7uYNvqtUvNxpXtugZ426b','1KOQXEz0FVzC8dF9_K14xh4PKH0EpzElS','1T9AidXj-Ho3ETnYrLI7Ot8bP5Q7zv8fL','1dQTT-uUbWxAxlgKbB-QanqdhbtMwu6PY','1dBxAqB-cStHo0MBgT3pxAetNpnRfCANK','1T67N5d3STuCmYI_a1XtEBWOWwyZW9XJO'];
